f6a5de116c54a77af05c995dcd70fdcfd54462dc
Wenbo <huwenbodut@gmail.com>
2018-10-03 17:48:45 +0800

    add server gpu info

--- gpuChecker.py
+++ gpuChecker.py
@@ -29,6 +29,46 @@ def parse_free_memory(output):
     assert (len(mo) % 2) == 0, "Cannot determine GPU numbers"
     return [int(a) for a in mo[::2]]
 
+def getServerInfo(serverID):
+    serverInfos = ['4*TITAN V',     #1
+                    '4*TITAN V',    #2
+                    '4*TITAN V',    #3
+                    '4*TITAN V',    #4
+                    '4*TITAN V',    #5
+                    '4*TITAN V',    #6
+
+                    '8*TITAN X',    #7
+                    '8*TITAN X',    #8
+                    '8*TITAN X',    #9
+
+                    '4*TITAN X',    #10
+                    '4*TITAN X',    #11
+                    '4*TITAN X',    #12
+                    '4*TITAN X',    #13
+                    '4*TITAN X',    #14
+                    '4*TITAN X',    #15
+                    '4*TITAN X',    #16
+
+                    '4*1080TI',     #17
+                    '4*1080TI',     #18
+                    '4*1080TI',     #19
+                    '4*1080TI',     #20
+                    '4*1080TI',     #21
+
+                    '4*TITAN V',    #22
+                    '4*TITAN V',    #23
+                    '4*TITAN V',    #24
+                    '4*TITAN V',    #25
+                    '4*TITAN V',    #26
+                    '4*TITAN V',    #27
+
+                    '4*TITAN XP',   #28
+                    '4*TITAN XP',   #29
+                    '4*TITAN XP',   #30
+                    '4*TITAN XP'    #31
+                ]
+    return serverInfos[serverID - 1]
+
 
 if __name__ == "__main__":
     with open('config.json', 'r') as f:
@@ -43,7 +83,7 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
             return None
-        return '[*] Server: gpu%s, Available gpus (ID): %s' % (server, res)
+        return '[*] Server: gpu%s, %s, Available gpus (ID): %s' % (server, getServerInfo(server), res)
 
 
     with futures.ProcessPoolExecutor(max_workers=31) as pool:
